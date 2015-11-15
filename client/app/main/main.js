cb2c69838068ed1e58ff30df3a76691ba753074d
Stan Sajous <stan@Normandy.local>
2015-11-14 23:20:31 -0700

    Scaffolded with angular-fullstack template and fixed some issues

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('timeIsMoneyApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
