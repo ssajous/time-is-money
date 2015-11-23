9016e7c836cb09d51ce2eb40731fbcfa288c161e
Stan Sajous <stan.sajous@gmail.com>
2015-11-22 21:21:35 -0700

    refactored time calculation

--- src/app/calendar/calendar.controller.js
+++ src/app/calendar/calendar.controller.js
@@ -8,42 +8,31 @@ export class CalendarController {
     this.totalAmount = 0;
     this.endDate.setMonth(this.endDate.getMonth() + 1);
     this.moment = moment;
+    this.days = [];
   }
 
   calculateTotal() {
-    let days = this.countWorkingDays();
+    this.buildWorkingDayList();
 
-    this.totalWorkingDays = days;
-    this.workingHours = days * this.hoursPerDay;
-
-    this.totalAmount = this.rate * this.hoursPerDay * days;
+    this.totalWorkingDays = this.days.length;
+    var day;
+    let hours = 0;
+    for (day of this.days) {
+      hours += day.hours;
+    }
+    this.workingHours = hours;
+    this.totalAmount = this.rate * this.workingHours;
   }
 
-  countWorkingDays() {
-    let start = this.moment(this.startDate);
+  buildWorkingDayList() {
+    let day = this.moment(this.startDate);
     let end = this.moment(this.endDate);
 
-    let first = start.clone().endOf('week'); // end of first week
-    let last = end.clone().startOf('week'); // start of last week
-    let days = last.diff(first, 'days') * 5 / 7; // this will always multiply of 7
-    let wfirst = first.day() - start.day(); // check first week
-
-    if (start.day() == 0) --wfirst; // -1 if start with sunday
-    let wlast = end.day() - last.day(); // check last week
-    if (end.day() == 6) --wlast; // -1 if end with saturday
-
-    this.days = this.buildWorkingDayList(start, end, this.hoursPerDay);
-
-    return wfirst + days + wlast;
-  }
-
-  buildWorkingDayList(start, end) {
-    let day = start;
-    let days = [];
-
+    this.days = [];
     while (day <= end) {
-      if (day.day() != 0 && day.day() != 6) {
-        days.push({
+      if (day.day() != 0 && day.day() != 6) { // exclude weekends
+        this.days.push({
+          day: day,
           label: day.format('dddd MMM Do'),
           hours: this.hoursPerDay,
           amount: this.hoursPerDay * this.rate
@@ -51,7 +40,5 @@ export class CalendarController {
       }
       day.add(1, 'days');
     }
-
-    return days;
   }
 }
